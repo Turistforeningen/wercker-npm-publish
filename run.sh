2ac404438debdc91c0239d10c16919c3bf44f140
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-06-11 15:59:35 +0200

    Double quote to prevent globbing

--- run.sh
+++ run.sh
@@ -19,19 +19,19 @@ if [ ! -f ./package.json ]; then
   exit 1
 fi
 
-echo _auth = ${NPM_AUTH_TOKEN} > ~/.npmrc
-echo email = ${NPM_EMAIL} >> ~/.npmrc
+echo _auth = "${NPM_AUTH_TOKEN}" > ~/.npmrc
+echo email = "${NPM_EMAIL}" >> ~/.npmrc
 
 NPM_VERSION=`cat package.json | grep 'version' | awk '{print substr($2, 2, length($2)-3)}'`
 NPM_VERSION_PRERELEASE=$(echo ${NPM_VERSION} | cut -d '-' -f 2 -s)
 
-echo "NPM_LATEST=${NPM_LATEST}"
-echo "NPM_VERSION=${NPM_VERSION}"
-echo "NPM_VERSION_PRERELEASE=${NPM_VERSION_PRERELEASE}"
+echo NPM_LATEST="${NPM_LATEST}"
+echo NPM_VERSION="${NPM_VERSION}"
+echo NPM_VERSION_PRERELEASE="${NPM_VERSION_PRERELEASE}"
 
 if [[ -n "${NPM_VERSION_PRERELEASE}" ]] || [[ "${NPM_LATEST}" != "true" ]]; then
-  echo npm publish --tag ${NPM_VERSION}
-  npm publish --tag ${NPM_VERSION}
+  echo npm publish --tag "${NPM_VERSION}"
+  npm publish --tag "${NPM_VERSION}"
 else
   echo npm publish
   npm publish
