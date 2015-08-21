b854590e6919a8cd45127f884975a03632629dd6
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-08-21 15:49:50 +0200

    Add preliminary step test script

---
+++ test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+  export EXIT_CODE=$1
+else
+  export EXIT_CODE=0
+fi
+
+function npm() {
+  echo "npm $@"
+  echo "npm ${EXIT_CODE}"
+  return ${EXIT_CODE}
+}
+
+function fail() {
+  echo "fail: $@"
+}
+
+function success() {
+  echo "success: $@"
+}
+
+function warn() {
+  echo "warn: $@"
+}
+
+function info() {
+  echo "info: $@"
+}
+
+export -f npm fail success warn info
+
+export NPM_EMAIL=foo@bar.com
+export NPM_AUTH_TOKEN=boobar123
+
+./run.sh
